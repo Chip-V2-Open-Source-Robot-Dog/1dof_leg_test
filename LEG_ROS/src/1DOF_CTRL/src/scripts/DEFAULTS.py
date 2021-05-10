ef918211b23431535f5d610896124b006cc207ec
adim <adim@mit.edu>
2021-05-10 13:05:12 -0400

    added networking

---
+++ LEG_ROS/src/1DOF_CTRL/src/scripts/DEFAULTS.py
@@ -0,0 +1,15 @@
+'''
+This class will allow us to store default parameters of the system!
+'''
+
+class DEFAULTS():
+    #NT Defaults
+    NT_DEFAULT_VAL = -1000000.0
+
+    #IP Addresses
+    RIO_IP = "10.20.20.2"
+    JETSON_IP = "10.20.20.12"
+
+    #epsilons for == testing & Defaults to NOT CHANGE
+    EPSILON = 1.0
+    
--- LEG_ROS/src/1DOF_CTRL/src/scripts/tester.py
+++ LEG_ROS/src/1DOF_CTRL/src/scripts/tester.py
@@ -1,10 +1,14 @@
+#!/usr/bin/env python
 
 #imports for CSV logging and such
 import csv
 from datetime import datetime as dt
 import os
+import time
 #import network tables
 from networktables import NetworkTables as nt
+#defaults class
+from DEFAULTS import DEFAULTS
 
 '''
 SETUP CSV LOGGING
@@ -15,10 +19,13 @@ script_dir = os.path.dirname(__file__)
 full_path = os.path.join(script_dir, filename)
 csv_file = open(full_path, mode='w')
 #setup the CSV file
-fieldnames = ['time', 'body_pos', 'fll_pos', 'frl_pos', 'bll_pos', 'brl_pos', 'orientation', 'reset'] # UPDATE THIS LATER
-writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-writer.writeheader()
-t0 = 0.0
+fieldnames = ["t", "VBUS",
+            "m1_p", "m2_p", "m1_current", "m2_current", 
+            "m1_v", "m2_v",
+            "m1_tau", "m2_tau"]
+writer = csv.writer(csv_file)
+writer.writerow(fieldnames)
+t0 = time.time()
 
 '''
 SOME INITIALIZATION CODE
@@ -29,8 +36,39 @@ nt.initialize(server=DEFAULTS.RIO_IP)
 sender=nt.getTable("rio_input")
 readout=nt.getTable("rio_readout")
 
+'''
+SETUP MODES DICT
+'''
+MODES = {1: 'VOL', 2: 'CUR', 3: 'VEL', 4: 'POS'}
+M1_DATA = [DEFAULTS.NT_DEFAULT_VAL]*4
+M2_DATA = [DEFAULTS.NT_DEFAULT_VAL]*4
+VBUS = DEFAULTS.NT_DEFAULT_VAL
+
 def write(command):
-    pass
+    #data from M1
+    sender.putNumber("m1_p", command[0])
+    sender.putNumber("m1_pd", command[1])
+    sender.putNumber("m1_tau", command[2])
+    sender.putNumber("m1_mode", command[3])
+    #data from M2
+    sender.putNumber("m2_p", command[4])
+    sender.putNumber("m2_pd", command[5])
+    sender.putNumber("m2_tau", command[6])
+    sender.putNumber("m2_mode", command[7])
+
+def read(data):
+    #data from M1
+    M1_DATA = [readout.getNumber("m1_p", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m1_v", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m1_tau", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m1_current", DEFAULTS.NT_DEFAULT_VAL)]
+    #data from M2
+    M2_DATA = [readout.getNumber("m2_p", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m2_v", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m2_tau", DEFAULTS.NT_DEFAULT_VAL), readout.getNumber("m2_current", DEFAULTS.NT_DEFAULT_VAL)]
+    #VBUS
+    VBUS = readout.getNumber("VBUS", DEFAULTS.NT_DEFAULT_VAL)
+    
+    #LOG THESE VALUES
+    t = time.time()-t0
+    DATA_VECTOR = [t]+[VBUS]+M1_DATA+M2_DATA
+    writer.writerow(DATA_VECTOR)
+
 
 
 '''
@@ -60,5 +98,10 @@ networking.addInputs(
 
 '''
 COMMAND:
+[m1_p, m1_pd, m1_tau, m1_mode, m2_p, m2_pd, m2_tau, m2_mode]
 
+MODE --> control mode (VEL, POS, CUR, VOL)
+
+RETURN DATA:
+[m1_p, m1_v, m1_tau, m1_current...
 '''
